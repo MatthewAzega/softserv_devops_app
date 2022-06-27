203ac6ae1712b02399fabc7edb4cb291fced3b10
Azega <56790000+MatthewAzega@users.noreply.github.com>
2022-06-27 18:23:29 +0200

    Update app.js

--- application/app.js
+++ application/app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('zmiana na latest'))
+app.get('/', (req, res) => res.send('nowy'))
 
 app.listen(port, () => console.log(`The app listening at http://localhost:${port}`))
