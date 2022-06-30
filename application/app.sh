2966e8b40b1ae22b87b2facad9ebfefd79941fcd
Azega <56790000+MatthewAzega@users.noreply.github.com>
2022-06-30 14:38:20 +0200

    Update app.sh

--- application/app.sh
+++ application/app.sh
@@ -1,4 +1,4 @@
 curl wttr.in/Wroclaw.png --output /var/www/html/Wroclaw.png
 echo "<img src="Wroclaw.png" alt="Wroclaw">" > /var/www/html/index.html
 curl wttr.in/Berlin.png --output /var/www/html/Berlin.png
-echo "<img src="Berlin.png" alt="Berlin">" > /var/www/html/index.html
+echo "<img src="Berlin.png" alt="Berlin">" >> /var/www/html/index.html
