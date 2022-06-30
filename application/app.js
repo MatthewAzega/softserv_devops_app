5a8c5c43f07b83492f44a7f828aefa1cd0b92786
Azega <56790000+MatthewAzega@users.noreply.github.com>
2022-06-30 13:59:19 +0200

    Delete app.js

--- application/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 8080
-
-app.get('/', (req, res) => res.send('helloworld'))
-
-app.listen(port, () => console.log(`The app listening at http://localhost:${port}`))
