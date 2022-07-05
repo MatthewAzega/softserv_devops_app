ccfa8b233ff407e7cdec22f158b2cfe968a4b82c
Matthewazega <matthewazega@gmail.com>
2022-07-05 15:32:50 +0200

    new app v2

--- application/app.sh
+++ application/app.sh
@@ -6,3 +6,4 @@ curl wttr.in/NewYork.png?mM --output /var/www/html/NewYork.png
 echo "<img src="NewYork.png" alt="NewYork">" >> /var/www/html/index.html
 curl wttr.in/Tokyo.png?mM --output /var/www/html/Tokyo.png
 echo "<img src="Tokyo.png" alt="Tokyo">" >> /var/www/html/index.html
+hostname >> /var/www/html/index.html
--- infrastructure/ecs_task_definition.tf
+++ infrastructure/ecs_task_definition.tf
@@ -1,5 +1,16 @@
 resource "aws_ecs_task_definition" "default" {
-  container_definitions    = file("containers_definitions.json")
+  container_definitions    = jsonencode([
+    {
+    cpu = 2,
+    image = "softserv_app",
+    name = "app",
+    memory = 256
+    portMappings = [
+      { containerPort = 8080
+        protocol = "tcp" }
+    ]
+    }
+  ])
   family                   = "development"
   memory                   = 1500
   network_mode             = "host"
