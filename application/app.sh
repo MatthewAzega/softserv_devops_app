1e5ce31c8728b6c33b975009f1c54689ddf45dba
Matthewazega <matthewazega@gmail.com>
2022-07-06 19:14:56 +0200

    v2.16

--- application/app.sh
+++ application/app.sh
@@ -1,6 +1,6 @@
 curl wttr.in/Wroclaw.png?mM --output /var/www/html/Wroclaw.png
 echo "<img src="Wroclaw.png" alt="Wroclaw">" > /var/www/html/index.html
-#curl wttr.in/Berlin.png?mM --output /var/www/html/Berlin.png
+##curl wttr.in/Berlin.png?mM --output /var/www/html/Berlin.png
 #echo "<img src="Berlin.png" alt="Berlin">" >> /var/www/html/index.html
 #curl wttr.in/NewYork.png?mM --output /var/www/html/NewYork.png
 #echo "<img src="NewYork.png" alt="NewYork">" >> /var/www/html/index.html
