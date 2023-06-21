7217636e19d3b83106e5902f44094f0abc0c3cc5
azega <matthewazega@gmail.com>
2023-06-21 11:54:15 +0200

    v3.00

--- application/app.sh
+++ application/app.sh
@@ -1,5 +1,4 @@
 curl wttr.in/Wroclaw.png?mM --output /var/www/html/Wroclaw.png
 echo "<img src="Wroclaw.png" alt="Wroclaw">" > /var/www/html/index.html
-curl wttr.in/Kiev.png?mM --output /var/www/html/Kiev.png
-echo "<img src="Kiev.png" alt="Kiev">" >> /var/www/html/index.html
+
 
