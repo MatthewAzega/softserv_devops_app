1f69023efcf0712347ad3f394d76d009265ac8f8
azega <matthewazega@gmail.com>
2023-06-29 11:07:04 +0200

    Added Poznan

--- application/app.sh
+++ application/app.sh
@@ -1,4 +1,5 @@
 curl wttr.in/Wroclaw.png?mM --output /var/www/html/Wroclaw.png
 echo "<img src="Wroclaw.png" alt="Wroclaw">" > /var/www/html/index.html
-
+curl wttr.in/Poznan.png?mM --output /var/www/html/Wroclaw.png
+echo "<img src="Wroclaw.png" alt="Wroclaw">" > /var/www/html/index.html
 
